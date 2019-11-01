d7f4e32e5d9a1067fd266799ad5d7c71c9ba10c5
Kerollyn <44269715+Kerollyn@users.noreply.github.com>
2019-11-01 19:36:20 -0300

    Add CadastroClientes

---
+++ CadastroCliente/FrmCadastro.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace CadastroCliente
+{
+    partial class FrmCadastro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNomeCompleto = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCPF = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSexo = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtNomeCompleto
+            // 
+            this.txtNomeCompleto.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.txtNomeCompleto.Location = new System.Drawing.Point(148, 64);
+            this.txtNomeCompleto.Name = "txtNomeCompleto";
+            this.txtNomeCompleto.Size = new System.Drawing.Size(362, 20);
+            this.txtNomeCompleto.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome Completo:";
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(209, 305);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 2;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(319, 305);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCadastrar.TabIndex = 3;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(260, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(204, 33);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Cadastro Cliente";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(106, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Cpf:";
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.txtCPF.Location = new System.Drawing.Point(148, 104);
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(160, 20);
+            this.txtCPF.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Data de Nascimento:";
+            // 
+            // txtData
+            // 
+            this.txtData.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.txtData.Location = new System.Drawing.Point(148, 191);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(160, 20);
+            this.txtData.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(356, 198);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Sexo:";
+            // 
+            // txtSexo
+            // 
+            this.txtSexo.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.txtSexo.Location = new System.Drawing.Point(410, 195);
+            this.txtSexo.Name = "txtSexo";
+            this.txtSexo.Size = new System.Drawing.Size(100, 20);
+            this.txtSexo.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(80, 241);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Telefone:";
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.txtTelefone.Location = new System.Drawing.Point(148, 234);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(160, 20);
+            this.txtTelefone.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(356, 111);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "E-mail:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.txtEmail.Location = new System.Drawing.Point(410, 108);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(257, 20);
+            this.txtEmail.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(47, 148);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Endereço:";
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.txtEndereco.Location = new System.Drawing.Point(148, 145);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(270, 20);
+            this.txtEndereco.TabIndex = 15;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(419, 305);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(103, 23);
+            this.btnLimpar.TabIndex = 17;
+            this.btnLimpar.Text = "Limpar Campos";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(443, 148);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(47, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Número:";
+            // 
+            // txtNum
+            // 
+            this.txtNum.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.txtNum.Location = new System.Drawing.Point(497, 145);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(100, 20);
+            this.txtNum.TabIndex = 18;
+            // 
+            // FrmCadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 340);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtSexo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCPF);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNomeCompleto);
+            this.Name = "FrmCadastro";
+            this.Text = "FrmCadastro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNomeCompleto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCPF;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSexo;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtNum;
+    }
+}
---
+++ CadastroCliente/FrmCadastro.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace CadastroCliente
+{
+    public partial class FrmCadastro : Form
+    {
+        public FrmCadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtNomeCompleto.Text != string.Empty && txtCPF.Text != string.Empty
+                    && txtData.Text != string.Empty && txtSexo.Text != string.Empty &&
+                    txtTelefone.Text != string.Empty && txtEmail.Text != string.Empty)
+                {
+
+                    //Instancio o SqlConnection, passando como parâmetro a string de conexão ao banco
+                    SqlConnection conn = new SqlConnection(@"Data Source=KEROLLYN\SQLEXPRESS01;Initial Catalog=Cadastro;
+                        Persist Security Info=True;User ID=sa;Password=Admin123");
+
+                    //Instancio o SqlCommand, responsável pelas instruções SQL e
+                    //Passo ao SqlCommand que a conexão que ele usará é o SqlConnection
+                    SqlCommand comm = new SqlCommand();
+                    comm.Connection = conn;
+
+                    //No CommandText do SqlCommand, passo a instrução SQL referente a inserção dos dados
+                    comm.CommandText = "INSERT INTO Clientes (Nome_Completo, Cpf, " +
+                                       " Data_Nasc, Sexo, Telefone, Email,Endereco,Numero) " +
+                                       //Nos Values, passo os valores referentes aos controles digitados pelo usuário
+                                       " VALUES (@Nome_Completo, @Cpf, @Data_Nasc, @Sexo, " +
+                                       "         @Telefone, @Email, @Endereco,@Numero) ";
+
+                    comm.Parameters.AddWithValue("@Nome_Completo", txtNomeCompleto.Text);
+                    comm.Parameters.AddWithValue("@Cpf", txtCPF.Text);
+                    comm.Parameters.AddWithValue("@Data_Nasc", txtData.Text);
+                    comm.Parameters.AddWithValue("@Sexo", txtSexo.ToString());
+                    comm.Parameters.AddWithValue("@Telefone", txtTelefone.Text);
+                    comm.Parameters.AddWithValue("@Email", txtEmail.Text);
+                    comm.Parameters.AddWithValue("@Endereco", txtEndereco.Text);
+                    comm.Parameters.AddWithValue("@Numero", txtNum.Text);
+
+                    //Abro a conexão, uso o método ExecuteNonQuery e fecho a conexão
+                    conn.Open();
+                    comm.ExecuteNonQuery();
+                    conn.Close();
+
+                    //Exibo ao usuário a mensagem de inserção efetuada com sucesso
+                    MessageBox.Show("Cadastro Realizado!", "Mensagem",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Informe os valores corretamente para completar o cadastro. ",
+                     "Erro do Sistema",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            var message = "Tem certeza que deseja encerrar a aplicação?";
+            var result = MessageBox.Show(message, "Encerrar", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtNomeCompleto.Clear();
+            txtCPF.Clear();
+            txtData.Clear();
+            txtEmail.Clear();
+            txtEndereco.Clear();
+            txtSexo.Clear();
+            txtTelefone.Clear();
+            txtNum.Clear();
+        }
+    }
+}
